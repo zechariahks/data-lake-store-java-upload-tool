92165875d44f9ea239a8bb9e106b3df062b764d6
gaswamin <Br1hasp@dh!>
2017-07-12 16:11:17 -0700

    Fixed a bug to make the JSON valid

--- adls-to-blob-cp/src/main/scala/com/starbucks/analytics/eventhub/Event.scala
+++ adls-to-blob-cp/src/main/scala/com/starbucks/analytics/eventhub/Event.scala
@@ -3,8 +3,8 @@ package com.starbucks.analytics.eventhub
 class Event(uri: String, sharedAccessSignatureToken: String) {
   def toJson: String =
     s"""{
-       |    uri: $uri,
-       |    sharedAccessSignatureToken: $sharedAccessSignatureToken
+       |    "uri": "$uri",
+       |    "sharedAccessSignatureToken": "$sharedAccessSignatureToken"
        |}
      """.stripMargin
 }
